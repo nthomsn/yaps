cf722b9577ef693c2fcbce7f356ae978c2a95731
Nicholas Thomson <neekthomson@gmail.com>
2015-10-06 19:39:46 -0400

    Make basic rails app

---
+++ vagrant.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get install -y git zlib1g-dev ruby-full build-essential nodejs sqlite3 libsqlite3-dev
+gem install rubygems-update
+gem install sqlite3
+gem install rails
+gem pristine --all
